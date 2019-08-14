f3ec11bed1c26b9b31ceed150247142f614613c9
Andriy Marushchak <differ.themes@gmail.com>
2019-08-14 15:33:58 +0300

    init v1.0

---
+++ app/assets/js/main.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function ($) {
+    "use strict";
+
+    const IOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream,
+        Android = navigator.userAgent.toLowerCase().indexOf("android") > -1,
+        Firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
+        Edge = navigator.userAgent.toLowerCase().indexOf('edge') > -1;
+
+    const browser = {
+        w: document.documentElement.clientWidth,
+        h: document.documentElement.clientHeight,
+    };
+
+    $(window).on('resize', () => {
+        browser.w = document.documentElement.clientWidth;
+        browser.h = document.documentElement.clientHeight;
+    });
+
+
+
+
+
+
+
+
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    notify = require('gulp-notify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create(),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    babel = require('gulp-babel'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify-es').default,
+    imagemin = require('gulp-imagemin'),
+    image = require('gulp-image');
+
+const PHPserver = false;
+const domain = 'localhost/gulp/app';
+const root = 'app';
+
+const paths = {
+    scss: {
+        src: 'app/assets/css/**/*.scss',
+        dest: 'app/assets/css'
+    },
+    js: {
+        src: 'app/assets/js/main.js',
+        dest: 'app/assets/js/'
+    },
+    img: {
+        src: 'app/assets/img/**/**/*',
+        dest: 'app/assets/img/'
+    }
+};
+
+function watch() {
+    gulp.watch(paths.scss.src, scss);
+    gulp.watch(paths.js.src, js);
+    gulp.watch("app/**/*.html", browserSync.reload);
+    gulp.watch("app/**/*.php", browserSync.reload);
+    gulp.watch("app/assets/js/**/**/*.js", browserSync.reload);
+    gulp.watch("app/assets/vendors/**/*", browserSync.reload);
+    gulp.watch(paths.img.src, browserSync.reload);
+}
+
+function server() {
+    if (PHPserver) {
+        browserSync.init({
+            proxy: domain,
+            notify: false
+        });
+    } else {
+        browserSync.init({
+            server: {
+                baseDir: root
+            },
+            notify: false
+        });
+    }
+}
+
+function js(done) {
+    gulp.src(paths.js.src)
+        .pipe(sourcemaps.init())
+        .pipe(babel({presets: ['@babel/env']}))
+        .pipe(rename("main.min.js"))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.js.dest));
+    done();
+}
+
+function scss(done) {
+    return gulp.src(paths.scss.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', notify.onError({
+            message: "<%= error.message %>",
+            title: "Sass Error!"
+        })))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.scss.dest))
+        .pipe(browserSync.stream());
+    done();
+}
+
+function img(done) {
+    gulp.src(paths.img.src)
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: true}
+                ]
+            })
+
+        ]))
+        .pipe(image())
+        .pipe(gulp.dest(paths.img.dest));
+    done();
+}
+
+exports.scss = scss;
+exports.js = js;
+exports.img = img;
+
+gulp.task('default', gulp.series(
+    gulp.parallel(scss, js),
+    gulp.parallel(server, watch)
+));
